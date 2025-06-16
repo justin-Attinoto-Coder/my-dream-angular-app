f19dbf43e6f02f0ebade14f1a7cf3f531825c36a
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-06-16 11:56:14 +0200

    feat: add TypeScript declaration for require.context in test setup

--- src/test.ts
+++ src/test.ts
@@ -2,6 +2,14 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
+// Declare require.context for TypeScript
+declare const require: {
+  context: (path: string, deep?: boolean, filter?: RegExp) => {
+    keys: () => string[];
+    <T>(id: string): T;
+  };
+};
+
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 const context = require.context('./', true, /\.spec\.ts$/);
 context.keys().map(context);
